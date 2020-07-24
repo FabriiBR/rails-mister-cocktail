5a097176e524fe65588e8bbd6dfe26d6e452ce97
Fabricio Butron <fa.butron.rodriguez@gmail.com>
2020-07-24 11:06:26 -0400

    arreglando bootstrap

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,40 @@
+class DosesController < ApplicationController
+  before_action :set_dose, only: [:show, :destroy]
+
+  def index
+    @doses = Dose.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
+    @dose.cocktail = @cocktail
+    @dose.ingredient = @ingredient
+    @dose.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+end
